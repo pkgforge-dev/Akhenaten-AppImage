3ba2ec8054f73583c180d4bca92efc32f62fa43b
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:53:13 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,8 +15,6 @@ export DEPLOY_VULKAN=1
 quick-sharun ./AppDir/bin/akhenaten
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
