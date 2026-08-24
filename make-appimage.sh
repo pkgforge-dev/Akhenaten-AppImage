5cabd8c83a8d4707646efc995df63ce2ce786f16
Link4Electronics <link4electronics@gmail.com>
2026-08-24 08:41:16 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,6 +7,7 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=https://raw.githubusercontent.com/dalerank/Akhenaten/refs/heads/master/res/akhenaten_256.png
 export STARTUPWMCLASS=akhenaten
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
