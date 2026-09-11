082f9fa65dc45be00ac31a4c83bdf9f791d5997a
Link4Electronics <link4electronics@gmail.com>
2026-09-10 21:34:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,10 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    cmake      \
-    libvpx     \
-    sdl2_mixer
+pacman -Syu --noconfirm cmake libvpx sdl2_mixer
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
